f5ddd20b1976fd9d5f19939ec6bedc9a60880e36
Vadim Shvetsov <shvetsov@amazon.com>
2023-11-20 17:08:21 +0100

    feat: Add replace to astronvim config

--- astronvim/user/mappings.lua
+++ astronvim/user/mappings.lua
@@ -20,6 +20,10 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>r"] = {
+      ":%s///g<Left><Left>",
+      desc = "Find and replace",
+    },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
     L = {
