dd275f0d0e2141c76befabd1732364e0728e8d29
Vadim Shvetsov <vadim@shvetsov.io>
2025-09-01 22:45:57 +0200

    chore: move extras to lazyvim.json

--- config/lazyvim/lua/config/lazy.lua
+++ config/lazyvim/lua/config/lazy.lua
@@ -18,16 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-
-    -- import extras
-    { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.lang.ansible" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-
     -- import/override with your plugins
     { import = "plugins" },
   },
